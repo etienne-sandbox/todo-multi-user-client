d61a472af9c5f056fa6233e2165f558afe53630a
Etienne Dldc <e.deladonchamps@gmail.com>
2021-01-27 14:01:27 +0100

    Fix ReactQueryDevtools

--- src/views/Root.tsx
+++ src/views/Root.tsx
@@ -5,7 +5,7 @@ import { Connect } from "./Connect";
 import { BrowserRouter as Router } from "react-router-dom";
 import { OverlayProvider } from "react-oot";
 import { LoadingView } from "./LoadingView";
-// import { ReactQueryDevtools } from "react-query-devtools";
+// import { ReactQueryDevtools } from "react-query/devtools";
 
 export const Root: FunctionComponent = () => {
   return (
